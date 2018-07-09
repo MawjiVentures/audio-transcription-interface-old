5d53841633d8d4ce4198cca1b2fd8afb88cf27b1
SENC275 <chensen275@gmail.com>
2018-07-09 12:06:52 -0400

    Add ability to edit text

--- src/segment.js
+++ src/segment.js
@@ -4,23 +4,30 @@ import TextHighlight from 'react-text-highlight'
 class Segment extends Component {
   constructor(props) {
     super(props)
+    this.clickCount = 0;
+    this.singleClickTimer = '';
     this.state = {
       chunk: props.chunk,
       callback: props.onClick,
-      highlight: ""
+      highlight: "",
+      isWritable: false,
+      text: props.chunk.text
     }
-    this.handleClick = this.handleClick.bind(this);
+    this.handleSingleClick = this.handleSingleClick.bind(this);
     this.handleHover = this.handleHover.bind(this);
     this.handleHoverOut = this.handleHoverOut.bind(this);
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
+    this.onValueChange = this.onValueChange.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
   }
   
-  handleClick() {
+  handleSingleClick() {
     this.state.callback(this.state.chunk);
   }
   
   handleHover(e) {
     this.setState({
-      highlight: this.state.chunk.text
+      highlight: this.state.text
     });
   }
   
@@ -29,17 +36,66 @@ class Segment extends Component {
       highlight: ""
     });
   }
+  
+  handleDoubleClick(e) {
+    this.setState({
+      isWritable: true
+    })
+  }
+  
+  onValueChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  handleClicks(){
+  	this.clickCount++;
+    if (this.clickCount === 1) {
+      this.singleClickTimer = setTimeout(function() {
+        this.clickCount = 0;
+        this.handleSingleClick();
+      }.bind(this), 300);
+
+    } else if (this.clickCount === 2) {
+      clearTimeout(this.singleClickTimer);
+      this.clickCount = 0;
+      this.handleDoubleClick();
+    }
+  }
+  
+  handleEnter(e) {
+    this.setState({
+      isWritable: false
+    })
+  }
 
   render() {
     return (
       <div onMouseOver={this.handleHover} 
-           onClick={this.handleClick}
+           onClick={() => this.handleClicks()}
            onMouseOut={this.handleHoverOut}
+           onDoubleClick={this.handleDoubleClick}
            >
-        <TextHighlight
+           
+        { !this.state.isWritable && <TextHighlight
           highlight={this.state.highlight}
-          text={this.state.chunk.text}
+          text={this.state.text}
         />
+        }
+        
+        { this.state.isWritable && 
+          <input name="text" 
+                 type='text' 
+                 value={this.state.text}
+                 onChange={this.onValueChange}
+                 onKeyPress={e => {
+                   if (e.key === 'Enter') {
+                     this.handleEnter(e)
+                   }
+                 }}
+                 ></input>
+        }
       </div>
     )
   }
