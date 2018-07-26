ee4189809e61cc9c8ad6b61fc08631fc0a6ab35d
SENC275 <chensen275@gmail.com>
2018-07-26 16:23:01 -0400

    Update highlight text method

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -2,37 +2,13 @@ import React, { Component } from 'react';
 import TextHighlight from 'react-text-highlight';
 
 class Highlight extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      highlight: ""
-    }
-  }
-
-  handleHover = (e) => {
-    this.setState({
-      highlight: this.props.text
-    });
-  }
-
-  handleHoverOut = (e) => {
-    setTimeout(() => {
-      this.setState({
-        highlight: ""
-      });
-    }, 800);
-  }
-
   render() {
     return (
-      <div className="highlight"
-           onClick={this.props.handleClick}
-           onMouseOver={this.handleHover}
-           onMouseOut={this.handleHoverOut}>
-        <TextHighlight
-          highlight={this.state.highlight}
-          text={this.props.text}
-          />
+      <div className="highlight">
+        <p className="highlight-text"
+           onClick={this.props.handleClick}>
+          {this.props.text}
+        </p>
       </div>
     )
   }
